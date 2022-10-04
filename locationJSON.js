06a1585dd387bc41f205eef2a3e549c15c1aaeb6
sumit-tokas <41648104+sumit-tokas@users.noreply.github.com>
2022-10-04 12:37:09 +0530

    Create locationJSON.js

---
+++ locationJSON.js
@@ -0,0 +1,14 @@
+var geography = [
+    {
+        cityName : "New Delhi",
+        state : "Delhi"
+    },
+    {
+        cityName : "Gurgaon",
+        state : "Haryana"
+    },
+    {
+        cityName : "Kolkata",
+        state : "West Bengal"
+    }
+]
